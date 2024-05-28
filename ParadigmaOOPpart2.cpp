8245e762220ca1bff01776f2332dae9bccccf99b
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2024-05-28 12:06:20 +0700

    membuat class joko

--- ParadigmaOOPpart2.cpp
+++ ParadigmaOOPpart2.cpp
@@ -16,3 +16,15 @@ public:
 		cout << "Pesan dari seseorang" << endl;
 	}*/
 };
+
+class joko : public seseorang
+{
+public:
+	// deklarasi
+	void pesan()
+	{
+		// implementasi
+		cout << "Pesan dari joko" << endl;
+	}
+};
+
